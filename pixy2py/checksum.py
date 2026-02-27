9efac3d18f84780eb15a6ceb8d1509c29072bc5f
Guilherme Samuel <guisamuel800@gmail.com>
2026-02-27 01:38:31 -0300

    Add docs into checksum class

--- pixy2py/checksum.py
+++ pixy2py/checksum.py
@@ -1,13 +1,25 @@
 class Checksum:
-    cs = 0
+    cs: int = 0
     @staticmethod
     def updateChecksum(b: int) -> None:
+        """Adds byte to checksum
+
+        :param b: Byte to be added
+        """
         cs += b
 
     @staticmethod
     def getChecksum() -> int:
+        """Returns calculated checksum
+
+        :returns: Calculated checksum
+        """
         return cs
 
     @staticmethod
     def reset() -> None:
+        """Returns calculated checksum
+
+        :returns: Calculated checksum
+        """
         cs = 0
